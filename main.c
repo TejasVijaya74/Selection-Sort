8b0043325fe302a4f4f427e0474774ce685d216e
TejasVijaya74 <vt6267700@gmail.com>
2024-07-13 19:40:20 +0530

    Intialized the code

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 /*Selection Sort
 Sort a given set of n integer elements using Selection Sort method and compute its time complexity. 
-Run the program for varied values of n> 5000 and record the time taken to sort. 
+Run the program for varied values of n>5000 and record the time taken to sort. 
 Plot a graph of the time taken versus n. The elements can be read from a file or can be generated using the random number generator.
 Demonstrate using C how the brute force method works along with its time complexity analysis: worst case, average case and best case.*/
 
